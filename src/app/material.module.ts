1575b78fe6fbc2c50c2f00f2f3b5793993209720
brendoncaulkins <brendon.caulkins@excella.com>
2019-06-06 10:51:38 -0400

    8: Add basic Material Navigation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,9 @@
+import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+
+import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
 
-const materialModules = [];
+const materialModules = [LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule];
 
 @NgModule({
   imports: [materialModules],
