1781906f83440591452167d03664397b0da1fd0b
brendoncaulkins <brendon.caulkins@excella.com>
2019-06-11 10:58:07 -0400

    employee-form: Add email and phone number to contact form FormGroup

--- src/app/employee/employee-form/contact-form/contact-form.component.ts
+++ src/app/employee/employee-form/contact-form/contact-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { BaseForm } from '../../abstracts/base-form.class';
-import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'tcp-contact-form',
@@ -10,18 +10,20 @@ import { FormGroup } from '@angular/forms';
 })
 export class ContactFormComponent extends BaseForm implements OnInit {
 
-
-  constructor() {
+  constructor(private fb: FormBuilder) {
     super();
     this.formGroup = this.buildForm();
   }
 
   ngOnInit() {
     this.emitFormReady();
-   }
+  }
 
   buildForm(): FormGroup {
-    return new FormGroup({});
+    return this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      phoneNumber: ['', [Validators.required, Validators.pattern(/\(\d{3}\)\d{3}-\d{4}/)]],
+    });
   }
 
 }
