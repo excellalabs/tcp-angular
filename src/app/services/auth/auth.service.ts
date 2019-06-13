688d19ca1963011a34012265f1c3beff8914bed2
brendoncaulkins <brendon.caulkins@excella.com>
2019-06-13 12:18:58 -0400

    auth: standardize code styling

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { Injectable } from '@angular/core'
 import { Router } from '@angular/router'
 import { JwtHelperService } from '@auth0/angular-jwt'
+
 import { Role } from '../../models/role'
 
 @Injectable({
