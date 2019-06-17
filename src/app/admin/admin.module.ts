5555c008d93c77c5855b37198991b2b10ec8cca1
brendoncaulkins <brendon.caulkins@excella.com>
2019-06-17 12:17:35 -0400

    tests: Remove unused component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,7 +5,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { MaterialModule } from '../material.module'
 import { PipeModule } from '../pipes/pipe.module'
-import { AdminPanelComponent } from './admin-panel/admin-panel.component'
 import { AdminRoutingModule } from './admin-routing.module'
 import { CategoryFormComponent } from './manage-categories/category-form/category-form.component'
 import { CategoryListComponent } from './manage-categories/category-list/category-list.component'
@@ -16,7 +15,6 @@ import { SkillListComponent } from './manage-skills/skill-list/skill-list.compon
 
 @NgModule({
   declarations: [
-    AdminPanelComponent,
     ManageSkillsComponent,
     ManageCategoriesComponent,
     SkillFormComponent,
