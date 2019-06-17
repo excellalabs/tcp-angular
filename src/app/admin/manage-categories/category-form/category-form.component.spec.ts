4ce06e7bd416e39bcbf4a470927fefe9cc2a7e2b
brendoncaulkins <brendon.caulkins@excella.com>
2019-06-17 15:13:35 -0400

    tests: CategoryFormComponent tests

--- src/app/admin/manage-categories/category-form/category-form.component.spec.ts
+++ src/app/admin/manage-categories/category-form/category-form.component.spec.ts
@@ -1,12 +1,16 @@
+import { DebugElement } from '@angular/core'
 import { ComponentFixture, TestBed, async } from '@angular/core/testing'
 import { FlexLayoutModule } from '@angular/flex-layout'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { AbstractControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { By } from '@angular/platform-browser'
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
-import { MaterialModule } from 'src/app/material.module'
-import { PipeModule } from 'src/app/pipes/pipe.module'
-import { SkillCategoriesService } from 'src/app/services/skill-categories/skill-categories.service'
-import { MockSkillCategoriesService } from 'src/app/services/skill-categories/skill-categories.service.fake'
+import { RequiredMessagePipe } from 'src/app/pipes/required-message/required-message.pipe';
 
+import { MaterialModule } from '../../../material.module'
+import { ICategory } from '../../../models/skill.interface'
+import { PipeModule } from '../../../pipes/pipe.module'
+import { SkillCategoriesService } from '../../../services/skill-categories/skill-categories.service'
+import { MockSkillCategoriesService } from '../../../services/skill-categories/skill-categories.service.fake'
 import { CategoryFormComponent } from './category-form.component'
 
 describe('CategoryFormComponent', () => {
@@ -38,5 +42,70 @@ describe('CategoryFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  describe('Name field', () => {
+    let nameField: AbstractControl
+    let error: DebugElement
+    beforeEach(() => {
+      nameField = component.formGroup.get('name')
+    })
+
+    it('should display the required error message', async () => {
+      expect(nameField.hasError('required')).toBe(true)
+      nameField.markAllAsTouched()
+      fixture.detectChanges()
+      await fixture.whenStable()
+      error = fixture.debugElement.query(By.css('.mat-error'))
+      expect(error.nativeElement.textContent.trim()).toEqual('Category Name' + RequiredMessagePipe.postfix)
+    })
+    it('should display the required error message', async () => {
+      expect(nameField.hasError('required')).toBe(true)
+      nameField.setValue('ab')
+      nameField.markAllAsTouched()
+      fixture.detectChanges()
+      await fixture.whenStable()
+      error = fixture.debugElement.query(By.css('.mat-error'))
+      expect(error.nativeElement.textContent.trim()).toEqual('Category Name must be at least 3 characters')
+    })
+  })
+
+  describe('Submit button', () => {
+    let button: DebugElement
+    beforeEach(() => {
+      button = fixture.debugElement.query(By.css('button#submit-category'))
+    })
+
+    it('should be disabled when the formGroup is invalid', () => {
+      expect(component.formGroup.invalid).toBe(true)
+      expect(button.nativeElement.disabled).toBe(true)
+    })
+    it('should be enabled when the formGroup is valid', async () => {
+      component.formGroup.get('name').setValue('Category Name')
+      fixture.detectChanges()
+      await fixture.whenStable()
+      expect(component.formGroup.valid).toBe(true)
+      expect(button.nativeElement.disabled).toBe(false)
+    })
+    it('should call onSubmit() when clicked', async () => {
+      spyOn(component, 'onSubmit').and.callThrough()
+      component.formGroup.get('name').setValue('Category Name')
+      fixture.detectChanges()
+      await fixture.whenStable()
+      button.nativeElement.click()
+      expect(component.onSubmit).toHaveBeenCalled()
+    })
+    it('should be labeled "Add Category" when not editing', async () => {
+      component.category = null
+      fixture.detectChanges()
+      await fixture.whenStable()
+      expect(button.nativeElement.textContent.trim()).toBe('Add Category')
+    })
+    it('should be labeled "Update Category" when not editing', async () => {
+      component.category = { id: 1, name: 'Sample Category' } as ICategory
+      fixture.detectChanges()
+      await fixture.whenStable()
+      expect(button.nativeElement.textContent.trim()).toBe('Update Category')
+    })
   })
 })
---
+++ src/app/admin/manage-categories/category-form/category-form.unit.spec.ts
@@ -0,0 +1,81 @@
+import { EventEmitter, SimpleChange, SimpleChanges } from '@angular/core';
+import { AbstractControl, FormBuilder } from '@angular/forms'
+import { ICategory } from 'src/app/models/skill.interface';
+
+import { CategoryFormComponent } from './category-form.component'
+
+describe('CategoryFormComponent (Unit)', () => {
+  let component: CategoryFormComponent
+  beforeEach(() => {
+    component = new CategoryFormComponent(new FormBuilder());
+  })
+
+  describe('constructor', () => {
+    it('should build the formGroup via #buildForm()', () => {
+      expect(component.formGroup).toBeTruthy()
+      expect(component.formGroup.contains('name'))
+    })
+    it('should have an addCategory EventEmitter', () => {
+      expect(component.addCategory instanceof EventEmitter).toBe(true)
+    })
+  })
+
+  describe('OnChanges', () => {
+    it('should reset the formGroup when category is set to null', () => {
+      spyOn(component.formGroup, 'reset').and.callThrough()
+      component.category = null
+      component.ngOnChanges({ category: new SimpleChange({} as ICategory, null, false)})
+      expect(component.formGroup.reset).toHaveBeenCalled()
+    })
+    it('should patch the formGroup when category is set to an object', () => {
+      spyOn(component.formGroup, 'patchValue').and.callThrough()
+      component.category = {id: 3, name: 'New Category'}
+      component.ngOnChanges({ category: new SimpleChange(null, component.category, false)})
+      expect(component.formGroup.patchValue).toHaveBeenCalledWith(component.category)
+    })
+    it('should do nothing if there isn\'t a real change', () => {
+      spyOn(component.formGroup, 'reset').and.callThrough()
+      spyOn(component.formGroup, 'patchValue').and.callThrough()
+      component.ngOnChanges({ category: new SimpleChange(null, null, false)})
+      expect(component.formGroup.reset).not.toHaveBeenCalled()
+      expect(component.formGroup.patchValue).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('#buildForm()', () => {
+    it('should have a name control', () => {
+      expect(component.buildForm().contains('name')).toBe(true)
+    })
+    describe('name field', () => {
+      let field: AbstractControl
+      beforeEach(() => {
+        field = component.formGroup.get('name')
+      })
+      it('should be required', () => {
+        expect(field.hasError('required')).toBe(true)
+      })
+      it('should enforce a 3 character minimum length', () => {
+        field.setValue('ab')
+        expect(field.hasError('minlength')).toBe(true)
+        expect(field.getError('minlength').requiredLength).toBe(3)
+      })
+    })
+  })
+
+  describe('#onSubmit()', () => {
+    it('should emit the new category via addCategory', () => {
+      spyOn(component.addCategory, 'emit').and.callThrough()
+      component.category = null
+      component.formGroup.get('name').setValue('New Category')
+      component.onSubmit()
+      expect(component.addCategory.emit).toHaveBeenCalledWith(jasmine.objectContaining({name: 'New Category'}))
+    })
+    it('should emit the updated category value via addCategory when editing', () => {
+      spyOn(component.addCategory, 'emit').and.callThrough()
+      component.category = {id: 15, name: 'Old Category'}
+      component.formGroup.get('name').setValue('New Category')
+      component.onSubmit()
+      expect(component.addCategory.emit).toHaveBeenCalledWith(jasmine.objectContaining({id: 15, name: 'New Category'}))
+    })
+  })
+})
