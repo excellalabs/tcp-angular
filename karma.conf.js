8535244dffc3720e26acab0e4d2ef844ebd01beb
Brendon Caulkins <32872899+brendoncaulkins@users.noreply.github.com>
2019-06-20 15:24:05 -0400

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,6 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-spec-reporter'),
-      require('karma-brief-reporter'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
