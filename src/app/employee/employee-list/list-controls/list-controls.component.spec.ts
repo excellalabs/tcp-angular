97102d26268e17d952ade148b2c5e79de5827b03
brendoncaulkins <brendon.caulkins@excella.com>
2019-06-20 13:08:40 -0400

    tests: Working ListControlsComponent spec config

--- src/app/employee/employee-list/list-controls/list-controls.component.spec.ts
+++ src/app/employee/employee-list/list-controls/list-controls.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/material.module';
+import { SkillsService } from 'src/app/services/skills/skills.service';
+import { MockSkillsService } from 'src/app/services/skills/skills.service.fake';
 
 import { ListControlsComponent } from './list-controls.component'
 
@@ -9,6 +15,10 @@ describe('ListControlsComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ListControlsComponent],
+      imports: [FormsModule, ReactiveFormsModule, FlexLayoutModule, MaterialModule, NoopAnimationsModule],
+      providers: [
+        { provide: SkillsService, useClass: MockSkillsService }
+      ]
     }).compileComponents()
   }))
 
