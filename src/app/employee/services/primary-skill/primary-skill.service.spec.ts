47e0828e6769126ae892fec1481d2225a8199f50
brendoncaulkins <brendon.caulkins@excella.com>
2019-06-24 14:26:24 -0400

    Fix broken test suite for PrimarySkillService

--- src/app/employee/services/primary-skill/primary-skill.service.spec.ts
+++ src/app/employee/services/primary-skill/primary-skill.service.spec.ts
@@ -3,7 +3,9 @@ import { TestBed } from '@angular/core/testing'
 import { PrimarySkillService } from './primary-skill.service'
 
 describe('PrimarySkillService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}))
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [PrimarySkillService]
+  }))
 
   it('should be created', () => {
     const service: PrimarySkillService = TestBed.get(PrimarySkillService)
