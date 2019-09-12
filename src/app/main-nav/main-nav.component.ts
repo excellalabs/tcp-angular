f3fdf392e3121cc2c913c47c2cd8c7997036ddb8
Justin Bartlett <justbartlett@gmail.com>
2019-09-12 14:53:50 -0400

    removed user welcome from body of the home page and added it to the top menu

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,7 +1,9 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
-import { Component } from '@angular/core'
-import { Observable } from 'rxjs'
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Observable, Subscription } from 'rxjs'
 import { map } from 'rxjs/operators'
+import { IEmployee } from '../models/employee.interface'
+import { EmployeesService } from '../services/employees/employees.service'
 
 import { AuthService } from '../services/auth/auth.service'
 
@@ -10,17 +12,30 @@ import { AuthService } from '../services/auth/auth.service'
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css'],
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit, OnDestroy {
+  employee$: Observable<IEmployee>
+  subscriptions: Subscription[] = []
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(map(result => result.matches))
 
   constructor(
+    private employeeService: EmployeesService,
     private breakpointObserver: BreakpointObserver,
     private authService: AuthService
   ) {}
 
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe())
+  }
+
   isLoggedIn() {
+    if (this.authService.isLoggedIn()) {
+      this.employee$ = this.getCurrentUser()
+    }
     return this.authService.isLoggedIn()
   }
 
@@ -30,5 +45,9 @@ export class MainNavComponent {
 
   currentUserIsAdmin() {
     return this.authService.isAdmin()
+  }
+
+  getCurrentUser(): Observable<IEmployee> {
+    return this.employeeService.getByEmail(this.authService.getEmail())
   }
 }
