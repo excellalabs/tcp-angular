8e687312e2b13f2b98099817a9c442b5cbb2f96d
Alex Hoffman <alexhoffman617@gmail.com>
2020-07-07 15:21:41 -0400

    pre-ng-update

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,29 +1,48 @@
 import { LayoutModule } from '@angular/cdk/layout'
 import { NgModule } from '@angular/core'
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatToolbarModule,
-} from '@angular/material'
+  import { MatAutocompleteModule } from '@angular/material/autocomplete'
+
+  import { MatButtonModule } from '@angular/material/button'
+
+  import { MatCardModule } from '@angular/material/card'
+
+  import { MatCheckboxModule } from '@angular/material/checkbox'
+
+  import { MatChipsModule } from '@angular/material/chips'
+
+  import { MatDatepickerModule } from '@angular/material/datepicker'
+
+  import { MatDialogModule } from '@angular/material/dialog'
+
+  import { MatFormFieldModule } from '@angular/material/form-field'
+
+  import { MatIconModule } from '@angular/material/icon'
+
+  import { MatInputModule } from '@angular/material/input'
+
+  import { MatListModule } from '@angular/material/list'
+
+  import { MatPaginatorModule } from '@angular/material/paginator'
+
+  import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+
+  import { MatRadioModule } from '@angular/material/radio'
+
+  import { MatSelectModule } from '@angular/material/select'
+
+  import { MatSidenavModule } from '@angular/material/sidenav'
+
+  import { MatSnackBarModule } from '@angular/material/snack-bar'
+
+  import { MatSortModule } from '@angular/material/sort'
+
+  import { MatStepperModule } from '@angular/material/stepper'
+
+  import { MatTableModule } from '@angular/material/table'
+
+  import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatNativeDateModule } from '@angular/material'
+
 
 const materialModules = [
   LayoutModule,
